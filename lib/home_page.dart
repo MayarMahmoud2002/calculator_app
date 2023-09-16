9cfe5a2bb64613be91b5811be4ac4f90bdad768b
Mayar Mahmoud <110978206+MayarMahmoud2002@users.noreply.github.com>
2023-09-16 11:09:15 +0300

    finish background for scaffold

---
+++ lib/home_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          height: double.infinity,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(colors: [
+              Color.fromRGBO(144, 197, 236, 1.0),
+              Color.fromRGBO(183, 179, 146, 1.0),
+            ],
+                begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+
+            ),
+          ),
+
+        ),
+
+      ),
+    );
+  }
+}
